a3ef9cb61c2db7bbce3594d8819906f82bfa3c4f
NickPalmucci <nicolas.eli.palmucci@gmail.com>
2021-05-12 13:48:52 -0400

    add first pass on color scale logic

--- src/planes/planes.js
+++ src/planes/planes.js
@@ -1,16 +1,20 @@
 import { Group } from 'three/src/objects/Group';
 import getPlane from '../plane/plane';
+import { setColorByScalar } from './utils'
 
 // import initPlaneGUI from '../plane/gui'
 
 export default function getPlanes (row) {
     const planes = new Group()
 
+    // the mutabiility of row here is shady
+    setColorByScalar(row, "scalarHeight")
+
     for (let i = 0; i <= row.length - 1; i++) {
-        const plane = getPlane({scalarHeight: row[i]})
+        const plane = getPlane(row[i])
 
         // hardcoded width set in plane.js
-        plane.position.set(i*2, 0, 0)
+        plane.position.set(i*16, 0, 0)
 
         planes.add(plane)
     }
--- src/planes/utils.js
+++ src/planes/utils.js
@@ -1,15 +1,40 @@
 
 // normalize array values between 0 and 1
-export const getNormalizedRowValues = (row) => {
-    const unsorted = Array.from(row)
-    const sorted = row.sort()
+export const getNormalizedRowValues = (row, scalarKey) => {
+    const rowData = row.map((item)=>{return item[scalarKey]})
+    const unsorted = Array.from(rowData)
+
+    const sorted = rowData.sort((a, b)=>{return a - b})
     const min = sorted[0]
     const max = sorted[row.length-1]
 
     const normalized = unsorted.map((i)=>{
-        // fix divide by zero 
+        if ((max-min) === 0) {return 0}
+
         return (i - min) / (max - min)
     })
 
     return normalized
+}
+
+export const setColorByScalar = (row, scalarKey) => {
+    const normalizedScalarValues = getNormalizedRowValues(row, scalarKey)
+
+    for (let i=0; i <= row.length-1; i++) {
+        const scalarByIndex = normalizedScalarValues[i]
+        row[i].color = getRgbByScalar(scalarByIndex)
+    }
+
+    return row 
+}
+
+const getRgbByScalar = (normScalar) => {
+    if (normScalar === 0) {return 0x49ef4}
+
+    const a = (1 - normScalar);
+    const Y = Math.floor(255 * a);
+    let r=255 
+    let g=Y 
+    let b=0
+    return "rgb("+r+","+g+","+b+")";
 }
