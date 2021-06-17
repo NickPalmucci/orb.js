e909b98fdea3fe6b7bcd4a41913b362fe9893491
NickPalmucci <nicolas.eli.palmucci@gmail.com>
2021-06-17 14:12:01 -0400

    add wireframe implementation with line segments

--- src/plane/plane.js
+++ src/plane/plane.js
@@ -1,14 +1,14 @@
 import { PlaneBufferGeometry } from 'three/src/geometries/PlaneBufferGeometry.js';
-import { MeshStandardMaterial } from 'three/src/materials/MeshStandardMaterial.js';
+import { MeshPhongMaterial } from 'three/src/materials/MeshPhongMaterial.js';
 import { Mesh } from 'three/src/objects/Mesh.js';
+import * as THREE from 'three';
 
 import { setMorph } from './morph'
 
-
 const getPlaneConfig = (options) => {
     const config = {
-        width: options.width || 2,
-        height: options.height || 2,
+        width: options.scalarArea || 16,
+        height: options.scalarArea || 16,
         widthSegments: 2,
         heightSegments: 2,
     };
@@ -29,19 +29,41 @@ export default function getPlane (options) {
 
     setMorph(geometry, options);
 
-    const material = new MeshStandardMaterial( {
-        color: 0x2194ce,
-        emissive: 0x2194ce,
-        flatShading: false,
-        wireframe: true,
-        morphTargets: true
+    const isVisible = true
+
+    const material = new MeshPhongMaterial( {
+        color: options.color || 0x2194ce,
+        side: 2, // DoubleSide
+        // emissive: options.color || 0x2194ce,
+        wireframe: options.wireframe || false,
+        morphTargets: true,
+        transparent: true,
+        opacity: 1,
+        visible: isVisible,
+        polygonOffset: true,
+        polygonOffsetFactor: 1, // positive value pushes polygon further away
+        polygonOffsetUnits: 1
     } );
 
     const mesh = new Mesh(geometry, material);
 
+    const geo = new PlaneBufferGeometry(
+        config.width,
+        config.height,
+        config.widthSegments,
+        config.heightSegments
+    );
+
+    setMorph(geo, options);
+
+    const mat = new THREE.LineBasicMaterial( { color: '#cdc9c9', morphTargets: true } );
+    const wireframe = new THREE.LineSegments( geo, mat );
+    mesh.add( wireframe );
+
     // set morph to max for scalarHeight representation
     // animate this at some point 
     mesh.morphTargetInfluences[ 0 ] = 1;
+    wireframe.morphTargetInfluences[ 0 ] = 1;
 
     return mesh
 };
