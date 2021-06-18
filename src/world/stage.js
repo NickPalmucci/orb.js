95ecdfbe32b92980e208f859e46aa526039e39da
NickPalmucci <nicolas.eli.palmucci@gmail.com>
2021-06-18 12:47:48 -0400

    calculate and position graph center on stage center,  get stage area by squaring larger graph side

--- src/world/stage.js
+++ src/world/stage.js
@@ -1,6 +1,8 @@
 import { PlaneBufferGeometry } from 'three/src/geometries/PlaneBufferGeometry.js';
 import { MeshPhongMaterial } from 'three/src/materials/MeshPhongMaterial';
 import { Mesh } from 'three/src/objects/Mesh.js';
+import { Vector3 } from 'three/src/math/Vector3';
+import { Box3 } from 'three/src/math/Box3';
 
 
 const getGroundDimensions = (dataArray) => {
@@ -25,7 +27,9 @@ const getGroundDimensions = (dataArray) => {
    let groundHeight = heightWithSpacers * scaleFactor
    let groundWith = (baseWidth * 16) * scaleFactor
 
-   return [groundHeight, groundWith]
+   let larger = groundHeight >= groundWith ? groundHeight  : groundWith
+
+   return [larger, larger]
 }
 
 // to build world we need three planes lined up as an open cube (stage)
@@ -37,15 +41,13 @@ export default function getStage(scene, dataArray, graph) {
 
     let coordiantes = [
         [groundHeight, groundWidth],
-        [groundHeight, groundWidth / 2],
-        [groundHeight / 2, groundWidth]
+        [groundHeight, groundWidth/2 ],
+        [groundHeight/2, groundWidth]
     ]
 
     const planes = coordiantes.map((tuple) => { return getStagePlane(tuple[0], tuple[1]) })
 
-    planes.forEach((mesh)=>{scene.add(mesh)})
-
-    planes[0].position.set(0, 0, 0)
+    planes.forEach((plane)=>{scene.add(plane)})
 
     planes[1].position.set(-groundWidth/2, 0, groundWidth/4)
     planes[1].rotation.y = Math.PI * 0.5;
@@ -53,12 +55,30 @@ export default function getStage(scene, dataArray, graph) {
     planes[2].position.set(0, groundHeight/2, groundHeight/4);
     planes[2].rotation.x = Math.PI * 0.5;
 
-    console.log(planes[0].position, planes[1].position, planes[1].rotation)
-
     planes[0].add(graph)
-    graph.position.set( -groundWidth/3 , -groundHeight/3, 10)    
 
+    const graphCenter = getCenterVec(graph)
+    const stageCenter = getCenterVec(planes[0])
+    const centerDiff = getCenterDistances(stageCenter, graphCenter)
+
+    graph.position.set(centerDiff.x, centerDiff.y, 10)
+  
     return planes
+}
+
+const getCenterVec = (object) => {
+    const boundingBox = new Box3().setFromObject(object)
+    const center = boundingBox.getCenter(new Vector3())
+
+    return center
+}
+
+const getCenterDistances = (stageCenter, graphCenter) => {
+    const x = stageCenter.x - graphCenter.x
+    const y = stageCenter.y - graphCenter.y
+    const distanceVec = new Vector3(x, y, 0)
+
+    return distanceVec
 }
 
 const getStagePlane = (height, width) => {
@@ -73,8 +93,3 @@ const getStagePlane = (height, width) => {
 
     return mesh
 }
-
-// const setStage = (sidePlanes, endPlanes) => {
-//     const
-
-// }
