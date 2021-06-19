093a516091f2a677a9f4fe9f4aa1b7010ac9f241
NickPalmucci <nicolas.eli.palmucci@gmail.com>
2021-06-19 18:24:28 -0400

    remove orb with skeleton experiment

--- src/orb/orb_skeleton.js
+++
@@ -1,9 +0,0 @@
-import {Skeleton} from 'three/src/objects/Skeleton.js';
-import getBonesArray from './orb_bones';
-
-export default (sizing) => {
-    const bones = getBonesArray(sizing);
-    const orbSkeleton  = new Skeleton(bones);
-
-    return [orbSkeleton, bones]
-}
